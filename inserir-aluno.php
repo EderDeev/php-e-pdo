fb60d334a83ffe2f200fcf7cb4e85069eac49496
Ederson <eder.dev1@gmail.com>
2024-10-03 11:36:39 -0300

    Evitando SQL injection

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -6,10 +6,17 @@ require 'Student.php';
 $caminhoAbsoluto = __DIR__ . '/banco.sqlite';
 $pdo  = new PDO('sqlite:'.$caminhoAbsoluto);
 
-$student = new Student(null,"Eder",new DateTimeImmutable('2000-08-19'));
+$student = new Student(null,"Aluno2';DROP TABLE student",new DateTimeImmutable('2000-08-19'));
 
-$sqlInsert = "INSERT INTO students(name,birthday) VALUES('{$student->getName()}', '{$student->getBirthday()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students(name,birthday) VALUES(?,?)";
+$stmt = $pdo->prepare($sqlInsert);
+$stmt->bindValue(1,$student->getName());
+$stmt->bindValue(2,$student->getBirthday()->format('Y-m-d'));
 
+if($stmt->execute()){
+    echo "Aluno registrado com sucesso";
+}
+exit();
 
 echo $sqlInsert;
 
